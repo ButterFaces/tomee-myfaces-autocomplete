726fb7fa7c7be3ffbb6080ed0d936bf59b946969
Lars Michaelis <mail@larmic.de>
2016-04-29 10:47:51 +0200

    first commit

---
+++ readme.md
@@ -0,0 +1,16 @@
+# ButterFaces - OmniFaces integration
+
+Simple example to use ButterFaces Autocomplete with TomEE 1.7.4 and MyFaces 2.1.17
+
+## Requirements
+Maven >= 3.2.1
+Java >= 8
+ButterFaces >= 2.1.0
+
+## Getting started
+
+```
+mvn clean package tomee:run
+```
+
+and open http://localhost:8080/tomee-test-1.0-SNAPSHOT/ and enter *te* for auto complete. 
---
+++ src/main/java/de/larmic/butterfaces/tomee/SimpleBean.java
@@ -0,0 +1,57 @@
+package de.larmic.butterfaces.tomee;
+
+import de.larmic.butterfaces.model.text.AutoCompleteModel;
+import de.larmic.butterfaces.util.StringUtils;
+
+import javax.annotation.PostConstruct;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Named
+@ViewScoped
+public class SimpleBean implements Serializable {
+
+    private final List<String> autoCompleteValues = new ArrayList<>();
+
+    private String value;
+
+    @PostConstruct
+    public void init() {
+        autoCompleteValues.add("test");
+        autoCompleteValues.add("tetest");
+        autoCompleteValues.add("test1 ButterFaces");
+        autoCompleteValues.add("test2");
+        autoCompleteValues.add("ButterFaces");
+        autoCompleteValues.add("ButterFaces JSF");
+        autoCompleteValues.add("ButterFaces Mojarra");
+        autoCompleteValues.add("ButterFaces Component");
+        autoCompleteValues.add("JSF");
+        autoCompleteValues.add("JSF 2");
+        autoCompleteValues.add("JSF 2.2");
+    }
+
+    public AutoCompleteModel getAutoCompleteModel() {
+        return value1 -> {
+            final List<String> values = new ArrayList<>();
+
+            if (StringUtils.isNotEmpty(value1.toString())) {
+                values.addAll(autoCompleteValues.stream()
+                        .filter(autoCompleteValue -> autoCompleteValue.toLowerCase().contains(value1.toString().toLowerCase()))
+                        .collect(Collectors.toList()));
+            }
+
+            return values;
+        };
+    }
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
